881cf26d6d455aed38bfd181eb088687b16a6d82
KimJinOh <jokim.dev@gmail.com>
2023-01-19 19:57:52 +0900

    비밀번호 설정중

--- src/Form/UserType.php
+++ src/Form/UserType.php
@@ -6,6 +6,7 @@ use App\Entity\User;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\EmailType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
@@ -15,9 +16,16 @@ class UserType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('email', EmailType::class)
-            ->add('password', PasswordType::class)
-        ;
+            ->add('email', EmailType::class, [
+                'help' => 'Email'
+            ])
+            ->add('password', RepeatedType::class, [
+                'type' => PasswordType::class,
+                'invalid_message' => 'The password fields must match.',
+                'required' => true,
+                'first_options'  => ['help' => 'Password'],
+                'second_options' => ['help' => 'Confirm Password'],
+            ]);
     }
 
     public function configureOptions(OptionsResolver $resolver): void
